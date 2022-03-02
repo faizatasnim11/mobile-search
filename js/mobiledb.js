f1aeb822c136df9e175ad50e2672970d01421928
faizatasnim11 <faizatasnim.ft@gmail.com>
2022-03-02 18:00:32 +0600

    bonus added

--- js/mobiledb.js
+++ js/mobiledb.js
@@ -1,6 +1,6 @@
-const searchMobile= () => {
-  const searchField= document.getElementById('search-field');
-  const searchText= searchField.value;
+const searchMobile = () => {
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
   console.log(searchText);
 
   searchField.value = '';
@@ -8,52 +8,55 @@ const searchMobile= () => {
 
   console.log(url);
   fetch(url)
-  .then(res => res.json())
-  .then(data => displayPhon(data.data));
+      .then(res => res.json())
+      .then(data => displayPhon(data.data));
   //.then(data => displaySearchResult(data.data));
 
 }
 const displayPhon = (data) => {
   const main = document.getElementById("main");
   console.log(main);
-  data.forEach(phon => {
-    // console.log(phon);
-     const div = document.createElement('div');
+  const limitedPhon = data.slice(0, 20);
+  
+  limitedPhon.forEach(phon => {
+      // console.log(phon);
+      const div = document.createElement('div');
 
-     div.classList.add("col-lg-4");
-     div.classList.add("mb-5");
-     div.innerHTML= ` 
-     <div class="card" style= "width: 18rem";>
-     <img src="${phon.image}" class="card-img-top" alt="...">
-     <div class="card-body">
-          <h5 class="card-title">${phon.phone_name}</h5>
-         <h6 class="card-title">${phon.brand} </h6>
-          <button onclick="phonDetails('${phon.slug}')" class="btn btn-primary">Details </button>
-     </div>
-   </div> `
-   main.appendChild(div);
- })
+      div.classList.add("col-lg-4");
+      div.classList.add("mb-5");
+      div.innerHTML = ` 
+   <div class="card" style= "width: 18rem";>
+   <img src="${phon.image}" class="card-img-top" alt="...">
+   <div class="card-body">
+        <h5 class="card-title">${phon.phone_name}</h5>
+       <h6 class="card-title">${phon.brand} </h6>
+        <button onclick="phonDetails('${phon.slug}')" class="btn btn-primary">Details </button>
+   </div>
+ </div> `
+      main.appendChild(div);
+  })
 }
 const phonDetails = (slug) => {
- fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
- .then(res => res.json())
- .then (data => console.log (data.data.mainFeatures))
+  fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+      .then(res => res.json())
+      .then(data => showPhone(data));
+}
+const showPhone = (phon) => {
+  console.log(phon);
+  const phonDetail = document.getElementById('phone-details')
+  phonDetail.innerHTML = '';
 
-
- const phonDetail = document.getElementById('phone-details')
- phonDetail.innerHTML = '';
- 
- const div = document.createElement('div');
- div.innerHTML=`
- <div class="card" style="width: 18rem;">
-     <img src="${phon.image}" class="card-img-top" alt="...">
-     <div class="card-body">
-         <h5 class="card-title">${mainFeatures.storage}</h5>
-         <p class="card-text">${mainFeatures.chipSet}</p>
-         <p class="card-text">${mainFeatures.memorey}</p>
-     </div>
- </div>
- `
- phonDetail.appendChild(div);
-
+  const div = document.createElement('div');
+  div.innerHTML = `
+<div class="card" style="width: 18rem;">
+   <img src="${phon.data.image}" class="card-img-top" alt="...">
+   <div class="card-body">
+       <h5 class="card-title">${phon.data.mainFeatures.storage}</h5>
+       <p class="card-text">${phon.data.mainFeatures.chipSet}</p>
+       <p class="card-text">${phon.data.mainFeatures.memory}</p>
+       <p class="card-text">${phon.data.mainFeatures.sensors}</p>
+   </div>
+</div>
+`
+  phonDetail.appendChild(div);
 }
